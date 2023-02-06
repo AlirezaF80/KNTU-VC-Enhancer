6b768e9efba9569a9bec5adb39339d25990651e0
AlirezaF80 <alirezafarzaneh@email.kntu.ac.ir>
2023-02-06 21:07:04 +0330

    Added Assignment Submission Status

--- KNTU VC Enhancer.user.js
+++ KNTU VC Enhancer.user.js
@@ -16,12 +16,21 @@
 // ==/UserScript==
 (function() {
     'use strict';
+    function getHTMLDoc(url){
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, false);
+        xhr.send();
+        var response = xhr.responseText;
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(response, "text/html");
+        return doc;
+    }
     function fixDropdownMenuWidth() {
         // find all divs with class="dropdown-menu ..." and set max-width=350px
         var divs = document.getElementsByClassName("dropdown-menu")
         // console.log(divs);
         for(var i = 0; i < divs.length; i++) {
-            divs[i].setAttribute("style", "max-width: 350px;");
+            divs[i].setAttribute("style", "max-width: 300px;");
         }
     }
     function addNoticeBoard(){
@@ -32,23 +41,12 @@
         var lis = document.getElementsByClassName("activity forum modtype_forum");
         if(lis.length > 0){
             var as = lis[0].getElementsByTagName("a");
-            if(as.length > 0){
-                noticeBoardUrl = as[0].href;
-            }
+            if(as.length > 0) noticeBoardUrl = as[0].href;
         }
-        if(noticeBoardUrl == null){
-            return;
-        }
-
+        if(noticeBoardUrl == null) return;
         // get the div with role="main" inside noticeBoardUrl
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", noticeBoardUrl, false);
-        xhr.send();
-        var response = xhr.responseText;
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(response, "text/html");
+        var doc = getHTMLDoc(noticeBoardUrl);
         var mainDiv = doc.querySelector("div[role='main']");
-        
         // add the mainDiv to the current page
         var mainDivs = document.querySelectorAll("div[class='course-content']");
         // add the mainDiv as the first child of the first mainDiv
@@ -96,12 +94,56 @@
         if(h1s.length == 0) return;
         h1s[0].innerHTML += " - " + teacherName;
     }
+    function addAssignmentsDetails(){
+        // first find all divs with class="activityinstance"
+        var divs = document.getElementsByClassName("activityinstance");
+        if (divs.length == 0) return;
+        // for each div, find the first a tag, get the href attribute
+        for(var i = 0; i < divs.length; i++){
+            var as = divs[i].getElementsByTagName("a");
+            if(as.length == 0) continue;
+            var url = as[0].href;
+            if (!url.includes('mod/assign')) continue;
+            // get the div with class="submissionstatustable" inside the url
+            var doc = getHTMLDoc(url);
+            var submissionstatustable = doc.getElementsByClassName("submissionstatustable");
+            if(submissionstatustable.length == 0) continue;
+            var trs = submissionstatustable[0].getElementsByTagName("tr");
+            if(trs.length < 4) continue;
+            var tds = trs[trs.length-3].getElementsByTagName("td");
+            if(tds.length == 0) continue;
+            var classList = tds[0].classList;
+            var backCol, fontCol;
+            if (classList.contains("earlysubmission")){
+                backCol = "#cfefcf";
+                fontCol = "#0f4d0f";
+            } else if (classList.contains("latesubmission")){
+                as[0].innerHTML += " (با تاخیر)";
+                backCol = "#ffff99";
+                fontCol = "#666600";
+            } else if (classList.contains("overdue")){
+                backCol = "#efcfcf";
+                fontCol = "#660000";
+            }
+            if (backCol != null && fontCol != null){
+                divs[i].style.backgroundColor = backCol;
+                as[0].style.color = fontCol;
+            }
+            // get the submission date
+            var tds2 = trs[trs.length-4].getElementsByTagName("td");
+            if(tds2.length == 0) continue;
+            var submissionDate = tds2[0].innerText;
+            // add submission date to the a tag
+            as[0].innerHTML += " - " + submissionDate;
+        }
+    }
     var url = window.location.href;
-    // if we are in a course's page
+    // if we are in a course page
     if(url.includes("course/view.php")){
         addTeacherName();
         addCourseDescription();
         addNoticeBoard();
+        addAssignmentsDetails();
     }
     fixDropdownMenuWidth();
 })();
